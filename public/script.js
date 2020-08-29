87bbf9c132a84794719480c6e9e7a7705681688f
sidhi-sid <sidhisidh469@gmail.com>
2020-08-30 00:06:40 +0530

    peer stream added

--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-
-
 const socket=io('/');
 const videoGrid=document.getElementById('video-grid');
 let myVideoStream;
@@ -14,10 +12,21 @@ var peer=new Peer(undefined,{
 
 navigator.mediaDevices.getUserMedia({
     video:true,
-    audio:true
+    audio:false
 }).then(stream=>{
     myVideoStream=stream;
     addVideoStream(myVideo,stream);
+
+    peer.on('call',function(call){
+        call.answer(stream);
+        console.log("call answered");
+        const video=document.createElement('video');
+        call.on('stream',myVideoStream=>{
+            addVideoStream(video,myVideoStream);
+            
+        })
+    })
+    
     socket.on('user-connected',(userId)=>{
         connectToNewUser(userId,stream);
     })
@@ -26,14 +35,13 @@ peer.on('open',id=>{
     socket.emit('join-room',Room_Id,id);
 })
 
-
-
-
 const connectToNewUser=(userId,stream)=>{
     const call=peer.call(userId,stream);
+    console.log("calling");
     const video=document.createElement('video');
     call.on('stream',userVideoStream=>{
         addVideoStream(video,userVideoStream);
+        
     })
 }
 
